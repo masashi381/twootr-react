749554580bfb0ffa9b7c7ee1adc68efc3904f819
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-07-15 16:02:56 -0700

    HistoryList showing history list

--- frontend/src/components/HistoryList.jsx
+++ frontend/src/components/HistoryList.jsx
@@ -1,9 +1,25 @@
+import axios from "axios";
 import HistoryPost from "./HistoryPost";
+import { useEffect, useState } from "react";
 
-export default function HIstoryList() {
+export default function HistoryList() {
+  const [postsList, setPostsList] = useState([])
+  useEffect(() => {
+    axios.get("http://localhost:8080/twoots")
+    .then((res) => {
+      console.log("serverData: " , res.data);
+      setPostsList(res.data);
+    })
+    .catch((err) => {
+      console.log("error: ", err);
+    });
+  },[]);
+  const parsedPost = postsList.map((post, index) => {
+    return <HistoryPost key={index} content={post}/>
+  })
   return (
     <div>
-      <HistoryPost />
+      {parsedPost}
     </div>
   )
 };
--- frontend/src/components/HistoryPost.jsx
+++ frontend/src/components/HistoryPost.jsx
@@ -1,7 +1,11 @@
-export default function HistoryPost() {
+export default function HistoryPost(props) {
+  console.log("props", props);
   return(
     <div>
-      <p>HistoryPost</p>
+      <h3>{props.content.author}</h3>
+      <h4>{props.content.authorSlug}</h4>
+      <p>{props.content.content}</p>
+      <p>{props.content.dateAdded}</p>
     </div>
   )
 };
