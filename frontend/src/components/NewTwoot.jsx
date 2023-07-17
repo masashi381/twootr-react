20143440b9d2b0b1f5bc5ede2014d041e0660120
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-07-17 11:26:36 -0700

    update submiBtn set validation

--- frontend/src/components/NewTwoot.jsx
+++ frontend/src/components/NewTwoot.jsx
@@ -3,21 +3,28 @@ import axios from "axios";
 
 export default function NewTwoot() {
   const [length, setLength] = useState(0);
+  
   const checkedInputLength = (event) => {
     setLength(event.target.value.length);
   }
   console.log(length);
   const submitBtn = (event) => {
     event.preventDefault();
-    axios.post("http://localhost:8080/twoot", {newTwoot: {
-      author:"Henry David Thoreau",
-      content:"Many men go fishing all of their lives without knowing that it is not fish they are after.",
-      authorSlug:"henry-david-thoreau",
-      dateAdded:"2022-07-06"
-  }})
-  .then((response) => {
-    console.log("here", response);
-  })
+    if(length === 0) {
+      alert("please enter");
+    } else if(length > 140) {
+      alert(" your message is too long");
+    }else {
+      axios.post("http://localhost:8080/twoot", {newTwoot: {
+        author:"Henry David Thoreau",
+        content:"Many men go fishing all of their lives without knowing that it is not fish they are after.",
+        authorSlug:"henry-david-thoreau",
+        dateAdded:"2022-07-06"
+    }})
+    .then((response) => {
+      console.log("here", response);
+    })
+   }
   };
   return (
   <div>
