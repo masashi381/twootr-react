eac939f114c491a8f55e7c7f24dd4291b21e9166
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-07-18 10:42:26 -0700

    update styledComponents part1

--- frontend/src/components/HistoryPost.jsx
+++ frontend/src/components/HistoryPost.jsx
@@ -1,3 +1,25 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+border: 1px solid #000;
+`;
+
+const FlexedName = styled.div`
+width: 90%;
+display: flex;
+justify-content: space-between;
+margin: 0 auto;
+`;
+
+const TextContent = styled.p`
+width: 95%;
+margin: 0 auto;
+border-bottom: 1px solid #000;
+`;
+
+const DateContent = styled.p`
+font-color: #888;
+`
 export default function HistoryPost(props) {
   // console.log("props", props);
   const checkedDate = () => {
@@ -15,11 +37,20 @@ export default function HistoryPost(props) {
     };
     
   return(
-    <div>
-      <h3>{props.content.author}</h3>
-      <h4>{props.content.authorSlug}</h4>
-      <p>{props.content.content}</p>
-      <p>{checkedDate()}</p>
-    </div>
+    <Container>
+      <FlexedName>
+        <h3>{props.content.author}</h3>
+        <h4>{props.content.authorSlug}</h4>
+      </FlexedName>
+      <TextContent>{props.content.content}</TextContent>
+      <FlexedName>
+        <DateContent>{checkedDate()}</DateContent>
+        <div>
+          <i className="fa-solid fa-flag"></i>
+          <i className="fa-solid fa-retweet"></i>
+          <i className="fa-solid fa-heart"></i>
+        </div>
+      </FlexedName>
+    </Container>
   )
 }
