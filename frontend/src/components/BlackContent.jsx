96c736447897822f116021d885ff61032d80a354
Kevin Lee <kevinlfcola@gmail.com>
2023-07-20 12:20:20 -0700

    Push the lastest vision of John Doe part for now

--- frontend/src/components/BlackContent.jsx
+++ frontend/src/components/BlackContent.jsx
@@ -1,5 +1,21 @@
-import formContent from "./formContent";
+import { useState } from "react";
+import FormContent from "./FormContent";
 
 export default function BlackContent() {
-    return <div>BlackContent</div>;
+    const [changeUserName, setChangeUserName] = useState(false);
+    const changeName = (event) => {
+        setChangeUserName(true);
+        event.preventDefault();
+    };
+
+    return (
+        <div>
+            <div>
+                <h3>John Doe</h3>
+                <button onClick={changeName}>Click</button>
+                {changeUserName && <FormContent />}
+            </div>
+            <p>@john-doe</p>
+        </div>
+    );
 }
