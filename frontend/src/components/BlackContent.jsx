aefb2b7e3f92a8dc6a8f833f988cd4a813aba7b1
Kevin Lee <kevinlfcola@gmail.com>
2023-07-21 11:15:29 -0700

    7/21 push the latest vision for now

--- frontend/src/components/BlackContent.jsx
+++ frontend/src/components/BlackContent.jsx
@@ -2,20 +2,38 @@ import { useState } from "react";
 import FormContent from "./FormContent";
 
 export default function BlackContent() {
+    //Account name
+    const [getUser, setGetUser] = useState("@john-doe");
+    const newUser = (newUser) => {
+        setGetUser(newUser);
+    };
+
+    //Title name
+    const [getNameTitle, setGetNameTitle] = useState("John Doe");
+    const newUserTitle = (newUserTitle) => {
+        setGetNameTitle(newUserTitle);
+    };
+
+    //Toggle the name change form when user click the button
     const [changeUserName, setChangeUserName] = useState(false);
     const changeName = (event) => {
-        setChangeUserName(true);
+        setChangeUserName(!changeUserName);
         event.preventDefault();
     };
 
     return (
         <div>
             <div>
-                <h3>John Doe</h3>
+                <h3>{getNameTitle}</h3>
                 <button onClick={changeName}>Click</button>
-                {changeUserName && <FormContent />}
+                {changeUserName && (
+                    <FormContent
+                        newUser={newUser}
+                        newUserTitle={newUserTitle}
+                    />
+                )}
+                <p>{getUser}</p>
             </div>
-            <p>@john-doe</p>
         </div>
     );
 }
